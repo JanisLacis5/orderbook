58b1f7bb6cff1c522bf4ce8c1186f3d9bcf4dc35
Janis Edvarts Lacis <janislacis06@gmail.com>
2025-10-13 19:40:40 +0300

    sketch the start of the OrderBook and Order classes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,58 @@
-int main() {
+#include <cstdint>
+#include <map>
+#include <vector>
 
+using price_t = std::int32_t;
+using quantity_t = std::uint32_t;
+using orderid_t = std::uint64_t;
+using orderids_t = std::vector<orderid_t>;
+
+enum OrderType {
+    Market,
+    GoodTillCancel,
+    GoodTillEOD,
+    FillOrKill,
+    FillAndKill
+};
+
+struct OrderInfo {
+    orderid_t orderid;
+    quantity_t quantity;
+    price_t price;
+    OrderType type;
+};
+
+class Order {
+public:
+    Order(orderid_t orderid, quantity_t quantity, price_t price, OrderType type)
+        : orderid_{orderid}, quantity_{quantity}, price_{price}, type_{type}
+    {}
+
+    orderid_t getOrderId() const { return orderid_; }
+    quantity_t getQuantity() const { return quantity_; }
+    price_t getPrice() const { return price_; }
+    OrderType getOrderType() const { return type_; }
+private:
+    orderid_t orderid_;
+    quantity_t quantity_;
+    price_t price_;
+    OrderType type_;
+};
+
+class OrderBook {
+public:
+    void openOrder(quantity_t quantity, price_t price, OrderType type);
+    void cancelOrder(orderid_t orderid);
+    void modifyOrder(orderid_t orderid, price_t price, quantity_t quantity, OrderType type);
+private:
+    orderid_t lastOrderId_{1};
+    std::map<price_t, orderids_t, std::less<price_t>> ask;
+    std::map<price_t, orderids_t, std::greater<price_t>> bid;
+
+    void incrementLastOrderId() { lastOrderId_++; };
+};
+
+int main() {
+    OrderBook orderbook;
+    return 0;
 }
