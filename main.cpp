3c1bb30196e2d5bf0c7139e9c37e96ac40435771
Janis Edvarts Lacis <janislacis06@gmail.com>
2025-10-19 22:51:05 +0300

    start addOrder function, create multiple helpers

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 
 using price_t = std::int32_t;
 using quantity_t = std::uint32_t;
-using orderid_t = std::uint64_t;
-using orderids_t = std::vector<orderid_t>;
-using ordertime_t = std::chrono::time_point<std::chrono::high_resolution_clock>;
+using orderId_t = std::uint64_t;
+using orderIds_t = std::vector<orderId_t>;
+using microsec_t = std::chrono::microseconds;
 
 enum OrderType {
     Market,
@@ -19,40 +19,133 @@ enum OrderType {
     FillAndKill
 };
 
-class Order {
-    Order(orderid_t orderid, quantity_t quantity, price_t price, OrderType type)
-        : orderid_{orderid}, quantity_{quantity}, price_{price}, type_{type}
-    {}
-private:
-    orderid_t orderid_;
-    quantity_t quantity_;
-    price_t price_;
-    OrderType type_;
-    time_t open_on;
+enum Side {
+    Buy,
+    Sell
 };
 
-using orderptr_t = std::shared_ptr<Order>;
-using orderptrs_t = std::list<orderptr_t>;
+struct ModifyOrder {
+    price_t price;
+    quantity_t quantity;
+    OrderType type;
+};
+
+class Order {
+public:
+    Order(orderId_t orderid, quantity_t quantity, price_t price, OrderType type, microsec_t opentime)
+        : orderid_{orderid}, initialQuantity_{quantity}, remainingQuantity_{quantity}, price_{price}, type_{type}, opentime_{opentime}
+    {}
+
+    orderId_t getOrderid() const { return orderid_; }
+    quantity_t getInitialQuantity() const { return initialQuantity_; }
+    quantity_t getRemainingQuantity() const { return remainingQuantity_; }
+    price_t getPrice() const { return price_; }
+    OrderType getType() const { return type_; }
+    Side getSide() const { return side_; }
+    microsec_t getOpenTime() const { return opentime_; }
+
+    void fill(quantity_t quantity) {
+        remainingQuantity_ -= quantity;
+    }
+    quantity_t getFilled() const { return initialQuantity_ - remainingQuantity_; };
+    bool isFullyFilled() const { return remainingQuantity_ == 0; }
+
+private:
+    orderId_t orderid_;
+    quantity_t initialQuantity_;
+    quantity_t remainingQuantity_;
+    price_t price_;
+    OrderType type_;
+    Side side_;
+    microsec_t opentime_;
+};
+
+using orderPtr_t = std::shared_ptr<Order>;
+using orderPtrs_t = std::list<orderPtr_t>;
+
+struct Trade {
+    orderPtr_t askTrade;
+    orderPtr_t bidTrade;
+};
+using trades_t = std::vector<Trade>;
 
 class OrderBook {
 public:
-    void newOrder(quantity_t quantity, price_t price, OrderType type);
-    void cancelOrder(orderid_t orderid);
-    void modifyOrder(orderid_t orderid, price_t price, quantity_t quantity, OrderType type);
+    trades_t addOrder(orderPtr_t order);
+    void cancelOrder(orderId_t orderId);
+    orderPtr_t modifyOrder(orderPtr_t order, ModifyOrder modifications);
 private:
     struct OrderInfo {
-        orderptr_t order_;
-        orderptrs_t::iterator location_;
+        orderPtr_t order_;
+        orderPtrs_t::iterator location_;
     };
 
-    orderid_t lastOrderId_{1};
-    std::map<price_t, orderids_t, std::less<price_t>> ask_;
-    std::map<price_t, orderids_t, std::greater<price_t>> bid_;
-    std::unordered_map<orderid_t, OrderInfo> orders_;
-    orderptrs_t orderPtrs_;
+    std::map<price_t, orderPtrs_t, std::less<price_t>> ask_;
+    std::map<price_t, orderPtrs_t, std::greater<price_t>> bid_;
+    orderPtrs_t orderPtrs_;
 
-    void incrementLastOrderId() { lastOrderId_++; };
+    trades_t matchOrders();
+    microsec_t getCurrTime() const {
+        auto time = std::chrono::system_clock::now().time_since_epoch();
+        return std::chrono::duration_cast<microsec_t>(time);
+    }
+    orderPtr_t toFillAndKill(orderPtr_t order) {
+        ModifyOrder modifications;
+        modifications.quantity = order->getRemainingQuantity();
+        modifications.price = order->getPrice();
+        modifications.type = OrderType::FillOrKill;
+        return modifyOrder(order, modifications);
+    }
+    void processAddedOrder(orderPtr_t order) {};
 };
+
+trades_t OrderBook::addOrder(orderPtr_t order) {
+    OrderType type = order->getType();
+    Side side = order->getSide();
+    price_t price = order->getPrice();
+
+    if (type == OrderType::Market) {
+        order = toFillAndKill(order);
+
+        if (side == Side::Sell) {
+            auto& [worstBidPrice, _] = *bid_.rbegin();
+            ask_[worstBidPrice].push_back(order);
+        }
+        else if (side == Side::Buy) {
+            auto& [worstAskPrice, _] = *ask_.rbegin();
+            bid_[worstAskPrice].push_back(order);
+        }
+        else
+            return {};
+    }
+
+    else if (type == OrderType::FillAndKill) {
+        if (side == Side::Sell) {
+            auto& [bestAsk, _] = *ask_.begin();
+
+            if (bestAsk <= order->getPrice())
+                return {};
+
+           ask_[order->getPrice()].push_back(order);
+        }
+        else if (side == Side::Buy) {
+            auto& [bestBid , _] = *bid_.begin();
+
+            if (bestBid >= order->getPrice())
+                return {};
+
+            bid_[order->getPrice()].push_back(order);
+        }
+    }
+    else if (type == OrderType::FillOrKill) {}
+    else if (type == OrderType::GoodTillCancel) {}
+    else if (type == OrderType::GoodTillEOD) {}
+    else
+        return {};
+
+    return matchOrders();
+}
+
 
 int main() {
     OrderBook orderbook;
