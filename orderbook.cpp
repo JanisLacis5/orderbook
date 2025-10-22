68444b89b109e1f24b4662b24f240e1617685b27
Janis Edvarts Lacis <janislacis06@gmail.com>
2025-10-22 15:03:22 +0300

    implement new functions

--- orderbook.cpp
+++ orderbook.cpp
@@ -1,4 +1,6 @@
+#include <numeric>
 #include "orderbook.h"
+
 
 // PRIVATE FUNCTION IMPLEMENTATIONS
 trades_t OrderBook::passiveMatchOrders() { // TODO: implement
@@ -22,22 +24,65 @@ orderPtr_t OrderBook::toFillAndKill(orderPtr_t order) {
     return modifyOrder(order, modifications);
 }
 
-void OrderBook::processAddedOrder(orderPtr_t order) {  // TODO: implement
-    return;
+void OrderBook::processAddedOrder(orderPtr_t order) {
+    orderId_t orderId = order->getOrderid();
+
+    orders_[orderId].order_ = order;
+    if (order->getSide() == Side::Sell) {
+        orders_[orderId].location_ = std::prev(ask_[order->getPrice()].end());
+    }
+    else if (order->getSide() == Side::Buy) {
+        orders_[orderId].location_ = std::prev(bid_[order->getPrice()].end());
+    }
+
+    levelData_[order->getPrice()].volume += order->getInitialQuantity();
+    levelData_[order->getPrice()].orderCnt++;
 }
 
-bool OrderBook::canBeFullyFilled(orderPtr_t order) const {  // TODO: implement
+bool OrderBook::canBeFullyFilled(price_t price, quantity_t quantity, Side side) const {
+    if (quantity <= 0)
+        throw std::logic_error("Invalid quantity");
+    if (!doesCrossSpread(price, side))
+        return false;
+
+    price_t treshold;
+    // respectively ask_ or bid_ are not empty - checked in doesCrossSpread function
+    if (side == Side::Sell) {
+        const auto& [bidPrice, _] = *bid_.begin();
+        treshold = bidPrice;
+    }
+    else if (side == Side::Buy) {
+        const auto& [askPrice, _] = *ask_.begin();
+        treshold = askPrice;
+    }
+
+    for (const auto& [levelPrice, levelData]: levelData_) {
+        if ((side == Side::Buy && levelPrice < treshold) || (side == Side::Sell && levelPrice > treshold))
+            continue;
+        if ((side == Side::Sell && levelPrice < price) || (side == Side::Buy && levelPrice > price))
+            continue;
+
+        if (quantity <= levelData.volume)
+            return true;
+        quantity -= levelData.volume;
+    }
     return false;
 }
 
-bool OrderBook::doesCrossSpread(orderPtr_t order) const {
-    if (order->getSide() == Side::Sell) {
+bool OrderBook::doesCrossSpread(price_t price, Side side) const {
+    if (side == Side::Sell) {
+        if (bid_.empty())
+            return false;
+
         const auto& [bestBid, _] = *bid_.begin();
-        return order->getPrice() <= bestBid;
+        return price <= bestBid;
     }
-    else if (order->getSide() == Side::Buy) {
+    else if (side == Side::Buy) {
+        if (ask_.empty())
+            return false;
+
         const auto& [bestAsk, _] = *ask_.begin();
-        return order->getPrice() >= bestAsk;
+        return price >= bestAsk;
     }
     else
         throw std::logic_error("Invalid side");
@@ -59,11 +104,11 @@ trades_t OrderBook::addOrder(orderPtr_t order) {
     OrderType type = order->getType();
 
     if (type == OrderType::FillAndKill) {
-        if (!doesCrossSpread(order))
+        if (!doesCrossSpread(order->getPrice(), order->getSide()))
             return {};
     }
     else if (type == OrderType::FillOrKill) {
-        if (!canBeFullyFilled(order) || !doesCrossSpread(order))
+        if (!canBeFullyFilled(order->getPrice(), order->getInitialQuantity(), order->getSide()))
             return {};
     }
     else if (type == OrderType::GoodTillCancel || type == OrderType::GoodTillEOD) {
@@ -75,7 +120,7 @@ trades_t OrderBook::addOrder(orderPtr_t order) {
 }
 
 void OrderBook::cancelOrder(orderId_t orderId) {  // TODO: implement
-    return;
+
 }
 
 orderPtr_t OrderBook::modifyOrder(orderPtr_t order, ModifyOrder modifications) {  // TODO: implement
--- orderbook.h
+++ orderbook.h
@@ -1,4 +1,5 @@
 #include <map>
+#include <unordered_map>
 #include "usings.h"
 #include "types.h"
 #include "trade.h"
@@ -13,16 +14,22 @@ private:
         orderPtr_t order_;
         orderPtrs_t::iterator location_;
     };
+    struct LevelData {
+        uint32_t volume = 0;
+        uint32_t orderCnt = 0;
+    };
 
     std::map<price_t, orderPtrs_t, std::less<price_t>> ask_;
     std::map<price_t, orderPtrs_t, std::greater<price_t>> bid_;
+    std::map<price_t, LevelData> levelData_;
+    std::unordered_map<orderId_t, OrderInfo> orders_;
 
     trades_t passiveMatchOrders();  // TODO: implement
     trades_t aggressiveMatchOrder(orderPtr_t order);  // TODO: implement
     microsec_t getCurrTime() const;
     orderPtr_t toFillAndKill(orderPtr_t order);
-    void processAddedOrder(orderPtr_t order);  // TODO: implement
-    bool canBeFullyFilled(orderPtr_t order) const;  // TODO: implement
-    bool doesCrossSpread(orderPtr_t order) const;
+    void processAddedOrder(orderPtr_t order);
+    bool canBeFullyFilled(price_t price, quantity_t quantity, Side side) const;
+    bool doesCrossSpread(price_t price, Side side) const;
     void addAtOrderPrice(orderPtr_t order);
 };
