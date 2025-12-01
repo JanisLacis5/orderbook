45be9c7ea23d20943d3c9b90e331e75c2c50fa48
Janis Edvarts Lacis <janislacis06@gmail.com>
2025-12-01 18:19:08 +0200

    remove pointer to Order in tests

--- tests/unit/test_order.cpp
+++ tests/unit/test_order.cpp
@@ -4,7 +4,7 @@
 #include "types.h"
 #include "usings.h"
 
-constexpr auto NOW = microsec_t{67};
+constexpr microsec_t NOW = microsec_t{67};
 
 class OrderTest : public testing::Test {
 protected:
@@ -15,24 +15,22 @@ protected:
     price_t price = 12345;
     OrderType type = OrderType::GoodTillCancel;
     Side side = Side::Sell;
-    Order* order = new Order(orderid, quantity, price, type, side, NOW);
+    Order order = Order(orderid, quantity, price, type, side, NOW);
 };
 
-// Test the constructor to use it as a fixture later
-TEST(Order, Constructor) {
-    Order* order = new Order(1, 100, 12345, OrderType::GoodTillCancel, Side::Sell, NOW);
-
-    EXPECT_EQ(order->getOrderid(), 1);
-    EXPECT_EQ(order->getInitialQuantity(), 100);
-    EXPECT_EQ(order->getRemainingQuantity(), 100);
-    EXPECT_EQ(order->getPrice(), 12345);
-    EXPECT_EQ(order->getType(), OrderType::GoodTillCancel);
-    EXPECT_EQ(order->getSide(), Side::Sell);
-    EXPECT_EQ(order->getOpenTime(), NOW);
+TEST_F(OrderTest, Constructor) {
+    EXPECT_EQ(order.getOrderid(), 1);
+    EXPECT_EQ(order.getInitialQuantity(), 100);
+    EXPECT_EQ(order.getRemainingQuantity(), 100);
+    EXPECT_EQ(order.getPrice(), 12345);
+    EXPECT_EQ(order.getType(), OrderType::GoodTillCancel);
+    EXPECT_EQ(order.getSide(), Side::Sell);
+    EXPECT_EQ(order.getOpenTime(), NOW);
 }
 
 TEST_F(OrderTest, FillFull) {
-    order->fill(quantity);
-    ASSERT_EQ(order->getInitialQuantity(), quantity);
-    EXPECT_EQ(order->getRemainingQuantity(), 0);
+    order.fill(quantity);
+
+    ASSERT_EQ(order.getInitialQuantity(), quantity);
+    EXPECT_EQ(order.getRemainingQuantity(), 0);
 }
