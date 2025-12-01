d049a3431f7b5c96f5074f51ec71d4f79324e7dd
Janis Edvarts Lacis <janislacis06@gmail.com>
2025-12-01 17:09:17 +0200

    start testing with gtest

---
+++ tests/unit/test_order.cpp
@@ -0,0 +1,20 @@
+#include <cassert>
+#include "order.h"
+#include "types.h"
+#include "usings.h"
+#include <gtest/gtest.h>
+
+
+TEST(Order, Constructor) {
+    auto now = microsec_t{67};
+    Order* order = new Order(1, 100, 12345, OrderType::GoodTillCancel, Side::Sell, now);
+
+    EXPECT_EQ(order->getOrderid(), 1);
+    EXPECT_EQ(order->getInitialQuantity(), 100);
+    EXPECT_EQ(order->getRemainingQuantity(), 100);
+    EXPECT_EQ(order->getPrice(), 12345);
+    EXPECT_EQ(order->getType(), OrderType::GoodTillCancel);
+    EXPECT_EQ(order->getSide(), Side::Sell);
+    EXPECT_EQ(order->getOpenTime(), now);
+}
+
