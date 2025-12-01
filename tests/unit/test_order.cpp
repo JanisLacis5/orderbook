bf68c44a3b353ab25be4953f6ec3e0838bba044f
Janis Edvarts Lacis <janislacis06@gmail.com>
2025-12-01 18:15:20 +0200

    create fixture for Order class testing

--- tests/unit/test_order.cpp
+++ tests/unit/test_order.cpp
@@ -4,9 +4,23 @@
 #include "types.h"
 #include "usings.h"
 
+constexpr auto NOW = microsec_t{67};
+
+class OrderTest : public testing::Test {
+protected:
+    OrderTest() {};
+    
+    orderId_t orderid = 1;
+    quantity_t quantity = 100;
+    price_t price = 12345;
+    OrderType type = OrderType::GoodTillCancel;
+    Side side = Side::Sell;
+    Order* order = new Order(orderid, quantity, price, type, side, NOW);
+};
+
+// Test the constructor to use it as a fixture later
 TEST(Order, Constructor) {
-    auto now = microsec_t{67};
-    Order* order = new Order(1, 100, 12345, OrderType::GoodTillCancel, Side::Sell, now);
+    Order* order = new Order(1, 100, 12345, OrderType::GoodTillCancel, Side::Sell, NOW);
 
     EXPECT_EQ(order->getOrderid(), 1);
     EXPECT_EQ(order->getInitialQuantity(), 100);
@@ -14,5 +28,11 @@ TEST(Order, Constructor) {
     EXPECT_EQ(order->getPrice(), 12345);
     EXPECT_EQ(order->getType(), OrderType::GoodTillCancel);
     EXPECT_EQ(order->getSide(), Side::Sell);
-    EXPECT_EQ(order->getOpenTime(), now);
+    EXPECT_EQ(order->getOpenTime(), NOW);
+}
+
+TEST_F(OrderTest, FillFull) {
+    order->fill(quantity);
+    ASSERT_EQ(order->getInitialQuantity(), quantity);
+    EXPECT_EQ(order->getRemainingQuantity(), 0);
 }
