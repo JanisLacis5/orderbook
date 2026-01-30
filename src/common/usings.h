e88756351d77716c3861ef02218378e7534bd5a8
Janis Edvarts Lacis <janislacis06@gmail.com>
2026-01-30 09:28:24 +0200

    make typedefs for the api

--- src/common/usings.h
+++ src/common/usings.h
@@ -9,3 +9,6 @@ using quantity_t = std::uint32_t;
 using orderId_t = std::uint64_t;
 using orderIds_t = std::vector<orderId_t>;
 using microsec_t = std::chrono::microseconds;
+
+using userId_t = std::uint64_t;
+using apiCallId_t = std::uint32_t;
