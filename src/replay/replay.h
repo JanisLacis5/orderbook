0c20b9dc1bff372e7c8b6bbe3808242d82c27f9d
Janis Edvarts Lacis <janislacis06@gmail.com>
2026-04-01 21:06:29 +0300

    create replay class initial interface

--- src/replay/replay.h
+++ src/replay/replay.h
@@ -1,5 +1,22 @@
-class replay {
+#include "file.h"
+
+class replay
+{
 public:
-    replay();
+    explicit replay(std::string inFp)
+        : inputFile_{inFp}
+    {
+    }
+    explicit replay(std::string inFp, std::string outFp)
+        : inputFile_{inFp}
+        , outputFile_{outFp}
+    {
+    }
     ~replay();
+
+    void run();
+
+private:
+    File inputFile_;
+    File outputFile_{"output.txt"};
 };
