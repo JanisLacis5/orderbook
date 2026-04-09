aad9be64522d19d6bdf5695c68d592181df68d94
Janis Edvarts Lacis <janislacis06@gmail.com>
2026-04-09 15:48:06 +0300

    formatting

--- src/common/strfuncs.h
+++ src/common/strfuncs.h
@@ -1,8 +1,9 @@
 #include <string>
 #include <vector>
 
-namespace strfuncs {
+namespace strfuncs
+{
     std::vector<std::string> split(const std::string& toSplit, const std::string sep, bool filterEmpty = true);
     std::string upper(const std::string& str);
     std::string lower(const std::string& str);
-}// namespace strfuncs
+} // namespace strfuncs
--- src/replay/replay.cpp
+++ src/replay/replay.cpp
@@ -1,15 +1,18 @@
 #include "replay.h"
 #include "strfuncs.h"
 
-Operation replay::parseLine(const std::string& raw) {
+Operation replay::parseLine(const std::string& raw)
+{
     auto tokens = strfuncs::split(raw, " ");
     if (tokens.empty())
         return {};
-    
-    auto action = tokens[0];    
-    if (action == "add") {}
-    if (action == "cancel") {}
-    if (action == "modify") {}
-    else 
+
+    auto action = tokens[0];
+    if (action == "add") {
+    }
+    if (action == "cancel") {
+    }
+    if (action == "modify") {
+    } else
         return {};
 }
