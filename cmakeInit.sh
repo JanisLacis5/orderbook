88981ad1b233c0672f0bb3954a1aaedde8e1c2ba
Janis Edvarts Lacis <janislacis06@gmail.com>
2026-04-12 16:24:52 +0300

    change compiler from clang to g++

--- cmakeInit.sh
+++ cmakeInit.sh
@@ -2,11 +2,9 @@ PROJECT_ROOT="$HOME/dev/orderbook/"
 
 cmake -S "$PROJECT_ROOT" -B "$PROJECT_ROOT/build/release" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
-    -DCMAKE_CXX_FLAGS="-stdlib=libc++"
+    -DCMAKE_CXX_COMPILER=/usr/bin/g++
 
 cmake -S "$PROJECT_ROOT" -B "$PROJECT_ROOT/build/debug" \
     -DENABLE_TSAN=ON \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
-    -DCMAKE_CXX_FLAGS="-stdlib=libc++"
+    -DCMAKE_CXX_COMPILER=/usr/bin/g++
