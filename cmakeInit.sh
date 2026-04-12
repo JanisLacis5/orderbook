5c41d1effabc990e57573ee200b44c7b1af0216d
Janis Edvarts Lacis <janislacis06@gmail.com>
2026-04-12 18:24:59 +0300

    use specific versions for compilers

--- cmakeInit.sh
+++ cmakeInit.sh
@@ -2,9 +2,9 @@ PROJECT_ROOT="$HOME/dev/orderbook/"
 
 cmake -S "$PROJECT_ROOT" -B "$PROJECT_ROOT/build/release" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_COMPILER=/usr/bin/g++
+    -DCMAKE_CXX_COMPILER=/usr/bin/g++-14
 
 cmake -S "$PROJECT_ROOT" -B "$PROJECT_ROOT/build/debug" \
     -DENABLE_TSAN=ON \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_CXX_COMPILER=/usr/bin/g++
+    -DCMAKE_CXX_COMPILER=/usr/bin/g++-14
