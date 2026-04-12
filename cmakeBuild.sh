cfede404fdc6a44ed835f6044cb2eef56cef1a93
Janis Edvarts Lacis <janislacis06@gmail.com>
2026-04-12 18:25:18 +0300

    add symlink to the root for compile_commands

--- cmakeBuild.sh
+++ cmakeBuild.sh
@@ -5,3 +5,5 @@ for dir in $buildDir; do
   echo "cmake --build $PROJECT_ROOT$dir"
   cmake --build "$PROJECT_ROOT$dir" -j$(nproc)
 done
+
+ln -sf $PROJECT_ROOT/build/release/compile_commands.json $PROJECT_ROOT/compile_commands.json
